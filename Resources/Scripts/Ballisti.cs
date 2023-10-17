98952f33fba4801a4b83008568865ba2dbb6b641
mika10095 <csml.csdsz@gmail.com>
2023-10-17 12:55:24 +0200

    eeh somewhat goodish...

--- Resources/Scripts/Ballisti.cs
+++ Resources/Scripts/Ballisti.cs
@@ -4,20 +4,26 @@ using System;
 public partial class Ballisti : Node3D
 {
 	float Velocity;
+	Vector3 Forward;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	   
+		 Forward = new Vector3(0, -1, 0);
+
 	}
 	public void Setup(float Velocity)
 	{
-		Velocity = this.Velocity;
+		this.Velocity = Velocity;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	
-			
+		var forward_dir = Basis * Forward;
+		Position += forward_dir * (float)delta * Velocity;
+
+
 	}
+	
 }
--- Resources/Scripts/Breech.gd
+++ Resources/Scripts/Breech.gd
@@ -12,13 +12,14 @@ var semiauto = true;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fp = get_node("../Barrel/FirePoint")
+	
 	env = EnviromentSetup
 func _spawm_bullet():
 	var bullet = load("res://bullet.tscn")
 	var projectile = bullet.instantiate()
 	projectile.global_transform = fp.global_transform
 	projectile.Setup(Velocity)
-	get_tree().root.add_child(projectile)
+	get_node("../../../WorldEnvironment").add_child(projectile)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(!Input.is_physical_key_pressed(KEY_ENTER)):
@@ -26,3 +27,4 @@ func _process(delta):
 	if(Input.is_physical_key_pressed(KEY_ENTER) && semiauto):
 		_spawm_bullet()
 		semiauto = false
+		
