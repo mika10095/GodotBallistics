c3392c75d3cc948ec8471eff824a709021767b0b
mika10095 <csml.csdsz@gmail.com>
2024-03-23 11:41:22 +0100

    fixed menu (again)

---
+++ Resources/CustomRes/SaveConfig/save_config.gd
@@ -0,0 +1,16 @@
+class_name SaveConfig extends Resource
+
+@export_range(0,1, .01) var master_audio: float = 1
+@export_range(0,1, .01) var music_audio: float = 1
+@export_range(0,1, .01) var effect_audio: float = 1
+@export_range(0,1, .01) var ui_audio: float = 1
+@export var window_setting: int = 0
+@export var fps_counter: bool = false
+func save() -> void:
+	ResourceSaver.save(self,"res://config.tres")
+
+static func load() -> SaveConfig:
+	var conf: SaveConfig = load("res://config.tres") as SaveConfig
+	if !conf:
+		conf = SaveConfig.new()
+	return conf
--- Resources/CustomRes/SaveData/save_data.gd
+++
@@ -1,1 +0,0 @@
-class_name save_data extends Resource
